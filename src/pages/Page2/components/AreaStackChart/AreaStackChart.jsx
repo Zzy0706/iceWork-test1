015d51fcbfecd754214b695b03e244f5ec03260b
Yu.Zhang <714794053@qq.com>
2018-06-01 11:28:08 +0800

    1.增加IP获取城市代码，2.增加获取数据错误的提醒窗口

--- src/pages/Page2/components/AreaStackChart/AreaStackChart.jsx
+++ src/pages/Page2/components/AreaStackChart/AreaStackChart.jsx
@@ -5,7 +5,7 @@ import { Dialog, Loading } from '@icedesign/base';
 import { Chart, Geom, Axis, Tooltip, Legend } from 'bizcharts';
 import IceContainer from '@icedesign/container';
 
-
+const API_GET_CITY = 'http://pv.sohu.com/cityjson';
 const API_URL = 'https://free-api.heweather.com/s6/weather/forecast?location=auto_ip&key=2e47737475a842669c731c29ebc1fd5a';
 export default class AreaStackChart extends Component {
   static displayName = 'AreaStackChart';
@@ -20,34 +20,56 @@ export default class AreaStackChart extends Component {
     };
   }
   componentDidMount() {
+    // this.getCityDta();
     this.loadLocalData(); // 获取缓存数据
     // this.getHttpData();
   }
   // 加载缓存，如果无缓存则加载网络数据
   loadLocalData() { // 获取缓存
-    const data = localStorage.getItem('HeWeather6');
-    if (data == null) {
-      this.getHttpData();
-      return;
+    try {
+      const data = localStorage.getItem('HeWeather6');
+      if (data == null) {
+        this.getHttpData();
+        return;
+      }
+      const localdataArray = JSON.parse(data);
+      const dataArray = [];
+      const now = moment().format();
+      // now.getFullYear.toString + '-' + now.getMonth.toString + '-' + now.getDay.toString;
+      // const dateStr = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+      if (localdataArray.update.loc.substring(0, 10) !== now.substring(0, 10)) {
+        this.getHttpData();
+        return;
+      }
+      localdataArray.daily_forecast.map(
+        item => dataArray.push(
+          { country: localdataArray.basic.location, date: item.date.slice(5), value: parseInt(item.tmp_max, 0) }
+        ));
+      this.setState({
+        data: dataArray,
+        visibleLoading: false,
+      });
+      return this.showView();
+    } catch (error) {
+      return this.setState({
+        visible: true,
+        dialogMessage: `${error.message}|${error.stack}`,
+      });
     }
-    const localdataArray = JSON.parse(data);
-    const dataArray = [];
-    const now = moment().format();
-    // now.getFullYear.toString + '-' + now.getMonth.toString + '-' + now.getDay.toString;
-    // const dateStr = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
-    if (localdataArray.update.loc.substring(0, 10) !== now.substring(0, 10)) {
-      this.getHttpData();
-      return;
-    }
-    localdataArray.daily_forecast.map(
-      item => dataArray.push(
-        { country: localdataArray.basic.location, date: item.date.slice(5), value: parseInt(item.tmp_max, 0) }
-      ));
-    this.setState({
-      data: dataArray,
-      visibleLoading: false,
+  }
+  getCityDta() {
+  /*  const instance = axios.create({
+      withCredentials: false,
+    }); */
+    axios(API_GET_CITY).then((response) => {
+      console.log(response);
+      return this.loadLocalData();
+    }).catch((error) => {
+      return this.setState({
+        visible: true,
+        dialogMessage: `${error.message}|${error.stack}`,
+      });
     });
-    return this.showView();
   }
   // 获取网络数据
   getHttpData() {
@@ -66,7 +88,7 @@ export default class AreaStackChart extends Component {
       console.log(error);
       this.setState({
         visible: true,
-        dialogMessage: error,
+        dialogMessage: `${error.message}|${error.stack}`,
       });
     });
     return this.showView();
@@ -107,8 +129,10 @@ export default class AreaStackChart extends Component {
             title="网络连接错误!请刷新重试!"
             footerAlign={this.state.footerAlign}
           >
-            <h3>获取天气数据失败</h3>
-            <span>{this.state.dialogMessage}123</span>
+            <div style={{ textAlign: 'center' }}>
+              <h3>获取天气数据失败</h3>
+              <span>{this.state.dialogMessage}</span>
+            </div>
           </Dialog>
         </span>
       </div>
